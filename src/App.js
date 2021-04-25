4504901cc040398f65ec9d8d7da701c826f51068
nirkaufman <nirkaufman@gmail.com>
2021-04-25 14:27:50 +0300

    more code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import Header from "./Header";
+import Footer from "./Footer";
+import Main from "./Main";
+import {useState} from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+
   return (
       <section className="todoapp">
-
-        <Header />
-
-
-        <section className="main">
-          <input className="toggle-all"
-                 type="checkbox" />
-            <ul className="todo-list">
-              <li>
-                <div className="view">
-                  <input className="toggle"
-                         type="checkbox" />
-                    <label>Todo Title</label>
-                  <button className="destroy" />
-                </div>
-                <input className="edit" />
-              </li>
-            </ul>
-        </section>
-
-        <footer className="footer">
-          <span className="todo-count"><strong>0</strong> items left</span>
-          <button className="clear-completed">Clear completed
-          </button>
-        </footer>
-
+        <Header title="Todobom"/>
+        <Main/>
+        <Footer/>
       </section>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 
-function Header () {
+function Header ({ title }) {
   return (
       <header className="header">
-        <h1>todos</h1>
+        <h1>{title}</h1>
         <input className="new-todo"
                placeholder="What needs to be done?"
                autoFocus />

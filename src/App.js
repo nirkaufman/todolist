a9c9462a40df74fc0cc86421aeec5d434e3ee1e4
nirkaufman <nirkaufman@gmail.com>
2021-04-25 14:03:43 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import {useState} from 'react';
+import Header from "./Header";
 
 function App() {
   return (
       <section className="todoapp">
 
-        <header className="header">
-          <h1>todos</h1>
-          <input className="new-todo"
-                 placeholder="What needs to be done?"
-                 autoFocus />
-        </header>
+        <Header />
+
 
         <section className="main">
           <input className="toggle-all"
